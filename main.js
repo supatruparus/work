2f71bc2cdf4f77b1dbdb04911ca52405b8d4795c
Ivan Yusupov <supatruparus@gmail.com>
2024-09-15 21:28:14 +0500

    sizing

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 !function (e, t) { "use strict"; var n = null, a = "PointerEvent" in e || e.navigator && "msPointerEnabled" in e.navigator, i = "ontouchstart" in e || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0, o = a ? "pointerdown" : i ? "touchstart" : "mousedown", r = a ? "pointerup" : i ? "touchend" : "mouseup", m = a ? "pointermove" : i ? "touchmove" : "mousemove", u = a ? "pointerleave" : i ? "touchleave" : "mouseleave", s = 0, c = 0, l = 10, v = 10; function f(e) { p(), e = function (e) { if (void 0 !== e.changedTouches) return e.changedTouches[0]; return e }(e), this.dispatchEvent(new CustomEvent("long-press", { bubbles: !0, cancelable: !0, detail: { clientX: e.clientX, clientY: e.clientY, offsetX: e.offsetX, offsetY: e.offsetY, pageX: e.pageX, pageY: e.pageY }, clientX: e.clientX, clientY: e.clientY, offsetX: e.offsetX, offsetY: e.offsetY, pageX: e.pageX, pageY: e.pageY, screenX: e.screenX, screenY: e.screenY })) || t.addEventListener("click", function e(n) { t.removeEventListener("click", e, !0), function (e) { e.stopImmediatePropagation(), e.preventDefault(), e.stopPropagation() }(n) }, !0) } function d(a) { p(a); var i = a.target, o = parseInt(function (e, n, a) { for (; e && e !== t.documentElement;) { var i = e.getAttribute(n); if (i) return i; e = e.parentNode } return a }(i, "data-long-press-delay", "1500"), 10); n = function (t, n) { if (!(e.requestAnimationFrame || e.webkitRequestAnimationFrame || e.mozRequestAnimationFrame && e.mozCancelRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame)) return e.setTimeout(t, n); var a = (new Date).getTime(), i = {}, o = function () { (new Date).getTime() - a >= n ? t.call() : i.value = requestAnimFrame(o) }; return i.value = requestAnimFrame(o), i }(f.bind(i, a), o) } function p(t) { var a; (a = n) && (e.cancelAnimationFrame ? e.cancelAnimationFrame(a.value) : e.webkitCancelAnimationFrame ? e.webkitCancelAnimationFrame(a.value) : e.webkitCancelRequestAnimationFrame ? e.webkitCancelRequestAnimationFrame(a.value) : e.mozCancelRequestAnimationFrame ? e.mozCancelRequestAnimationFrame(a.value) : e.oCancelRequestAnimationFrame ? e.oCancelRequestAnimationFrame(a.value) : e.msCancelRequestAnimationFrame ? e.msCancelRequestAnimationFrame(a.value) : clearTimeout(a)), n = null } "function" != typeof e.CustomEvent && (e.CustomEvent = function (e, n) { n = n || { bubbles: !1, cancelable: !1, detail: void 0 }; var a = t.createEvent("CustomEvent"); return a.initCustomEvent(e, n.bubbles, n.cancelable, n.detail), a }, e.CustomEvent.prototype = e.Event.prototype), e.requestAnimFrame = e.requestAnimationFrame || e.webkitRequestAnimationFrame || e.mozRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame || function (t) { e.setTimeout(t, 1e3 / 60) }, t.addEventListener(r, p, !0), t.addEventListener(u, p, !0), t.addEventListener(m, function (e) { var t = Math.abs(s - e.clientX), n = Math.abs(c - e.clientY); (t >= l || n >= v) && p() }, !0), t.addEventListener("wheel", p, !0), t.addEventListener("scroll", p, !0), t.addEventListener("contextmenu", p, !0), t.addEventListener(o, function (e) { s = e.clientX, c = e.clientY, d(e) }, !0) }(window, document);
 class Params {
-  constructor(w, l, qty, boxes) {
+  constructor(w, h, qty, boxes) {
     this.w = w,
-      this.l = l,
+      this.l = h,
       this.qty = qty,
       this.boxes = boxes
   }
@@ -14,9 +14,9 @@ class Tile {
     this.height = height
   }
 }
-
-const input_l = document.getElementById('length')
-const input_w = document.getElementById('width')
+const maxSize = 300
+const input_height = document.getElementById('length')
+const input_width = document.getElementById('width')
 const input_qty = document.getElementById('qty')
 const input_boxes = document.getElementById('boxes')
 const output = document.getElementById('result')
@@ -29,7 +29,7 @@ const tilesListElem = document.getElementById('tiles')
 let params = new Params(0, 0, 1, 1);
 let tilesList = []
 
-const inputs = [input_l, input_w, input_qty, input_boxes]
+const inputs = [input_height, input_width, input_qty, input_boxes]
 inputs.forEach((elem) => elem.addEventListener('focus', () => {
   elem.select();
 })
@@ -54,13 +54,13 @@ inputs.forEach((elem) => {
 })
 
 
-input_l.addEventListener('input', () => {
+input_height.addEventListener('input', () => {
   params.l = event.target.value
   showParams()
   output.value = '-'
 })
 
-input_w.addEventListener('input', () => {
+input_width.addEventListener('input', () => {
   params.w = event.target.value
   showParams()
   output.value = '-'
@@ -98,8 +98,8 @@ btn_calc.addEventListener('click', () => {
 
 
 function addTile(width, height) {
+  console.log(`width = ${width}, height = ${height}`)
   tilesList.push(new Tile(width, height))
-  console.log(tilesList)
   const newTile = document.createElement('button')
   newTile.className = 'tile'
 
@@ -120,43 +120,32 @@ function addTile(width, height) {
     console.log('delete')
     tilesListElem.removeChild(newTile)
     tilesList.splice(tilesList.indexOf(new Tile(width, height)))
-
-
-
   }
-
-
-
-
-
   newTile.addEventListener('click', () => {
-    console.log()
-    input_l.value = height
-    input_w.value = width
+    input_height.value = height
+    input_width.value = width
     output.value = '-'
-
-    params.w = width
-    params.l = height
-
-    newTile.style.backgroundColor = '#ffffff29'
-    // newTile.style.color = 'black'
-    newTile.style.borderWidth = '4px'
-    // newTile.style.fontSize = 'larger'
-    // newTileElem.style.display = 'none'
-
+    // params.w = width
+    // params.l = height
     getSiblings(newTile).forEach((elem) => {
       elem.style.backgroundColor = ''
     })
   })
   newTile.classList.add(['newTile'])
-  if (width > height) {
-    newTile.style.width = `${width / width * 300}px`
-    newTile.style.height = `${height / width * 300}px`
-    console.log('width>length')
+  let result = Number(width)>Number(height)
+  console.log(`${width} >= ${height} ? : ${result}`)
+  console.log(typeof width)
+  if (result) {
+    console.log('width>=height')
+
+    newTile.style.width = `${width / width * maxSize}px`
+    newTile.style.height = `${height / width * maxSize}px`
   } else {
-    newTile.style.width = `${height / height * 300}px`
-    newTile.style.height = `${width / height * 300}px`
+    console.log('width < height')
+    newTile.style.width = `${(height / height * maxSize)}px`
+    newTile.style.height = `${(width / height * maxSize)}px`
   }
+  console.log(`style = width: ${newTile.style.width} height: ${newTile.style.height}`)
 
   tilesListElem.insertBefore(newTile, btn_showAddTile)
 }
