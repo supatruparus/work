a0e5360120febf2755d575dddec93f395e14dbc7
supatruparus <95300675+supatruparus@users.noreply.github.com>
2024-09-17 04:09:18 +0500

    Update main.js

--- main.js
+++ main.js
@@ -167,8 +167,8 @@ function calcS(params) {
   if (params.qty == '' || params.qty == 0) {
     params.qty = 1
   }
-  let result = params.l * params.w * params.qty * params.boxes + pie / 1000000
-
+  let result = params.l * params.w * (params.qty * params.boxes + pie) / 1000000
+  console.log(params)
   return result
 
 
